dccaa5997f530b76e5c9f00e729932c5e1330338
ashleygraf101 <ashley.graf101@gmail.com>
2020-07-30 18:28:33 +1000

    use invoke for second test

--- cypress/integration/laragram/laragram.spec.js
+++ cypress/integration/laragram/laragram.spec.js
@@ -8,26 +8,26 @@ beforeEach(() => {
     .should('have.value', 'demo@gmail.com')
   cy.get('#password')
     .type('123456')
+    .should('have.value', '123456')
   cy.contains('Log In')
     .click()
 })
 
 describe('Home Page', () => {
   it('Visits the login page and logs in', () => {
-      cy.contains('Laravel').should('be.visible')
+      cy.contains('Laravel')
   })
 })
 
 describe('Feed Page', () => {
   it('Add photo to feed', () => {
-    cy.contains('Laravel').should('be.visible')
     cy.get('input[type="file"]').invoke('show')
       .click({force: true})
       .attachFile(yourFixturePath)
       .click({force: true})
     cy.contains('Next').click()
     cy.get('.description-container')
-      .type('hello, is it me you are looking for?')
+      .type('hello, it is me')
     cy.contains('Share').click()
   })
 })
